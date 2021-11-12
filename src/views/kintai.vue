b1ef7108811776671f16607a9a116ff647d46964
Shoya-Osada <s.oauspicious@gmail.com>
2021-11-12 16:58:22 +0900

    変更２

--- src/views/kintai.vue
+++ src/views/kintai.vue
@@ -12,7 +12,6 @@
         <tr v-for="(item,index) in worklist" :key="index">
           <td v-if="item.data.number == 0">{{item.data.date}}  {{item.data.time}}</td>
           <td>{{item.data.date}}  {{item.data.time}}</td>
-          <!-- <td>{{item.data.number}}</td> -->
         </tr>
       </table>
     </div>
@@ -30,9 +29,11 @@ export default {
   },
   methods:{
     start_work(){
+      //現在の年月日時を取得
       var start_date = new Date().toLocaleDateString()
       var start_time = new Date().toLocaleTimeString()
       console.log(start_date,start_time)
+      //データベースにデータを送る
       axios.post("https://fir-795b6-default-rtdb.firebaseio.com/user.json",{
         data:{
           date:start_date,
@@ -47,9 +48,11 @@ export default {
     },
 
     end_work(){
+      //現在の年月日時を取得
       var end_date = new Date().toLocaleDateString()
       var end_time = new Date().toLocaleTimeString()
       console.log(end_date,end_time)
+      //データベースにデータを送る
       axios.post("https://fir-795b6-default-rtdb.firebaseio.com/user.json",{
         data:{
           date:end_date,
@@ -64,6 +67,7 @@ export default {
 
     },
     async datalist(){
+      //データベースからデータを取得
      var box = await axios.get("https://fir-795b6-default-rtdb.firebaseio.com/user.json")
      this.worklist = box.data
      console.log(this.worklist)
